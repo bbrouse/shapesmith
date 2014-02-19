455fbb100c5bb39cb132ea8768e6763bc76ea247
unknown <andrew.staffieri@gmail.com>
2014-02-19 13:31:24 -0500

    Added a script for gravity so we don't have to use rigidbodies.  It's not very good, because it operates off the lowest block relative to the rest of the shape, so certain orientations still gravitate through objects.  I was thinking about trying shooting a ray from the blocks that face the ground, and calculate the distance to the nearest object.  If the distance is 0, stop moving.

---
+++ shapesmith/Assets/Scripts/BlockGravity.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGravity : MonoBehaviour {
+	public GameObject tetromino;
+	public GameController gameController;
+
+	private bool gravityOn = true;
+	private int invoke = 0;
+	private bool allowTranslate = false;
+	private Vector3[] tetBlocks = new Vector3[4];
+	private bool[] lowestBlock = new bool[4];
+	private float lowestPos = 100000;
+
+	void Start(){
+		for (int i=0; i<4; i++) {
+			tetBlocks[i] = new Vector3(1000, 1000, 1000);
+			
+			tetBlocks[i] = tetromino.transform.GetChild(i).position;
+
+			if(tetBlocks[i].y < lowestPos) lowestPos = tetBlocks[i].y;
+		}
+	}
+
+	void Update () {
+		if (gravityOn == true) {
+			for (int i=0; i<4; i++) {
+				if (tetBlocks[i].y > 0) invoke++;
+				if(tetBlocks[i].y == lowestPos) lowestBlock[i] = true;
+			}
+
+			if(invoke == 4){
+				invoke = 0;
+				gravityOn = false;
+				InvokeRepeating("tetrominoGravity", .5f, .25f);
+			}
+		}
+	}
+
+	void tetrominoGravity(){
+		for (int i=0; i<4; i++) {
+			if(lowestBlock[i] == true){
+				tetBlocks[i].y -= 1;
+				if(gameController.checkObjectProximity(tetBlocks[i]) && tetBlocks[i].y >= 0){
+					allowTranslate = true;
+				}else{
+					allowTranslate = false;
+					break;
+				}
+			}
+		}
+
+		if (allowTranslate == true) {
+			tetromino.transform.Translate (Vector3.up * -1, Space.World);
+			allowTranslate = false;
+		} else {
+			CancelInvoke();
+		}
+	}
+}
