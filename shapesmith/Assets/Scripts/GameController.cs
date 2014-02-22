fe85f91ea4c2bf946297c48f0ad90081f7cca425
unknown <andrew.staffieri@gmail.com>
2014-02-22 14:50:36 -0500

    Added functionality to toggle allowance of manual shape switching with the toggle of debug mode.  Added a function and functionality to randomize tetrominos every 7.5 seconds when not in debug mode.  Modularized a reset tetromino function which resets placeholder meshes to their original position and rotation.

--- shapesmith/Assets/Scripts/GameController.cs
+++ shapesmith/Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Update(){
-		if (Input.GetKeyDown ("tab")) {
+		if (Input.GetKeyDown ("tab") && debugMode) {
 			placementManager.switchTetromino();
 		}
 		
