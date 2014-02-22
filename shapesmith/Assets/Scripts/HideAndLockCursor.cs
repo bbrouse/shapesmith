c80ac8e0a61c86a0d71588ee87c62004ccc445bd
unknown <andrew.staffieri@gmail.com>
2014-02-22 13:56:52 -0500

    Added a simple preliminary script to handle pausing the game.  Removed pause handling from HideAndLockCursor.c

--- shapesmith/Assets/Scripts/HideAndLockCursor.cs
+++ shapesmith/Assets/Scripts/HideAndLockCursor.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class HideAndLockCursor : MonoBehaviour {
 	public GameObject player;
+	public GameObject gameController;
+
 	private CharacterMotor playerMotor;
 	private MouseLook playerMouse;
 	private MouseLook cameraMouse;
+	private PlacementManager placementManager;
 
 	//Starts with the cursor locked and grabs the components for player movement and player/camera mouse movement.
 	void Start() {
@@ -13,15 +16,15 @@ public class HideAndLockCursor : MonoBehaviour {
 		playerMotor = player.GetComponent<CharacterMotor> ();
 		playerMouse = player.GetComponent<MouseLook> ();
 		cameraMouse = Camera.main.GetComponent<MouseLook> ();
+		placementManager = gameController.GetComponent<PlacementManager> ();
 	}
 
 	//Sets all movement and mouse locks to false if true and true if false on pressing the p key.
-	void Update() {
-		if (Input.GetKeyDown ("p")) {
-			Screen.lockCursor = !Screen.lockCursor;
-			playerMotor.enabled = !playerMotor.enabled;
-			playerMouse.enabled = !playerMouse.enabled;
-			cameraMouse.enabled = !cameraMouse.enabled;
-		}
+	public void toggleCursorLock() {
+		Screen.lockCursor = !Screen.lockCursor;
+		playerMotor.enabled = !playerMotor.enabled;
+		playerMouse.enabled = !playerMouse.enabled;
+		cameraMouse.enabled = !cameraMouse.enabled;
+		placementManager.enabled = !placementManager.enabled;
 	}
 }
---
+++ shapesmith/Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseHandler : MonoBehaviour {
+	public HideAndLockCursor cursorController;
+
+	private bool paused = false;
+	
+	void Update()
+	{
+		if (Input.GetKeyDown ("p")) {
+			cursorController.toggleCursorLock ();
+			paused = !paused;
+		}
+	}
+	
+	void OnGUI()
+	{
+		if(paused)
+		{
+			GUILayout.Label("Game is paused!");
+			if(GUILayout.Button("Click me to unpause")){
+				cursorController.toggleCursorLock();
+				paused = !paused;
+			}
+		}
+	}
+	
+	bool togglePause()
+	{
+		if(Time.timeScale == 0f)
+		{
+			Time.timeScale = 1f;
+			return(false);
+		}
+		else
+		{
+			Time.timeScale = 0f;
+			return(true);    
+		}
+	}
+}
