a703b8318c7f2e4f295ddc52412ec49b5234eca4
unknown <andrew.staffieri@gmail.com>
2014-02-22 13:35:18 -0500

    Added functionality to lock and hide the cursor on playing the game, and show it when paused.  Also includes functionality to freeze the player when paused.

---
+++ shapesmith/Assets/Scripts/HideAndLockCursor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideAndLockCursor : MonoBehaviour {
+	public GameObject player;
+	private CharacterMotor playerMotor;
+	private MouseLook playerMouse;
+	private MouseLook cameraMouse;
+
+	//Starts with the cursor locked and grabs the components for player movement and player/camera mouse movement.
+	void Start() {
+		Screen.lockCursor = true;
+		playerMotor = player.GetComponent<CharacterMotor> ();
+		playerMouse = player.GetComponent<MouseLook> ();
+		cameraMouse = Camera.main.GetComponent<MouseLook> ();
+	}
+
+	//Sets all movement and mouse locks to false if true and true if false on pressing the p key.
+	void Update() {
+		if (Input.GetKeyDown ("p")) {
+			Screen.lockCursor = !Screen.lockCursor;
+			playerMotor.enabled = !playerMotor.enabled;
+			playerMouse.enabled = !playerMouse.enabled;
+			cameraMouse.enabled = !cameraMouse.enabled;
+		}
+	}
+}
