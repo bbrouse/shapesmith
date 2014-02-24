163dde9a677faa23e09f9d5c0e99b60633f83f3e
Andrew S <andrew.staffieri@gmail.com>
2014-02-24 02:25:55 -0500

    Updated the prefabs, they zonked out somehow.  Updated gameController's new logic to allow debug mode.  Added fonts.  Set up a guiText object.

--- shapesmith/Assets/Scripts/GameController.cs
+++ shapesmith/Assets/Scripts/GameController.cs
@@ -14,10 +14,11 @@ public class GameController : MonoBehaviour {
 	public int finalTimeLeft;
 
 	void Start(){
-		debugMode = false;
 		tetrominoTimeLeft = tetrominoTimeLimit;
 		finalTimeLeft = finalTimeLimit;
-		InvokeRepeating("tetrominoTimerCountdown", 1.0f, 1.0f);
+		if(!debugMode){
+			InvokeRepeating("tetrominoTimerCountdown", 1.0f, 1.0f);
+		}
 	}
 
 	void Update(){
@@ -50,6 +51,7 @@ public class GameController : MonoBehaviour {
 		}
 		else{
 			placementManager.placeTetromino();
+			return;
 		}
 		guiText.text = tetrominoTimeLeft.ToString ();
 		tetrominoTimeLeft = tetrominoTimeLimit;
