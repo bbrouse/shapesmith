f6dfe3f5ef7b26a9b1fd6a3f1c86354b6326b836
Ben Brouse <benjaminbrouse@gmail.com>
2014-02-24 21:07:21 -0500

    Added Level 3 with moving platform

---
+++ shapesmith/Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformTrigger : MonoBehaviour {
+
+	public float platformSpeed;
+	private bool triggered = false;
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("Trigger entrered");
+		if (other.collider.tag == "Player" && !triggered) {
+			Debug.Log("Start platform");
+			transform.parent.animation["level_2_moving_platform"].speed = platformSpeed;
+			transform.parent.animation.Play("level_2_moving_platform");
+		}
+
+		triggered = true;
+	}
+}
