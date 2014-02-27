a93edb41275f3873856cead5b4e01b21737dd1d0
unknown <andrew.staffieri@gmail.com>
2014-02-27 15:04:24 -0500

    Adding a handler for wireframe offsets so it doesn't blend with other colliders.

---
+++ shapesmith/Assets/Scripts/wireframeOffsethandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class wireframeOffsethandler : MonoBehaviour {
+	public float maxDistance;
+	public LayerMask environmentMask;
+
+	private GameObject origin;
+	private List<Ray> placementRays = new List<Ray>();
+	private RaycastHit hitInfo;
+
+	void Update () {
+		origin = this.transform.parent.gameObject;
+
+		placementRays.Add(new Ray (origin.transform.position, origin.transform.forward));
+		placementRays.Add(new Ray (origin.transform.position, -origin.transform.forward));
+		placementRays.Add(new Ray (origin.transform.position, origin.transform.up));
+		placementRays.Add(new Ray (origin.transform.position, -origin.transform.up));
+		placementRays.Add(new Ray (origin.transform.position, origin.transform.right));
+		placementRays.Add(new Ray (origin.transform.position, -origin.transform.right));
+
+		for(int i=0; i<6; i++){
+			if(Physics.Raycast(placementRays[i], out hitInfo, maxDistance, environmentMask)){
+				origin.transform.Translate((hitInfo.normal * .01f), Space.World);
+			}
+		}
+	}
+}
