629ee468a2ed40fe44ac848dac64f6e018675de6
Ben Brouse <benjaminbrouse@gmail.com>
2014-03-03 17:50:13 -0500

    Added level entrances, added tetros in the sky falling

---
+++ shapesmith/Assets/Scripts/SkyTetrominoSpawning.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SkyTetrominoSpawning : MonoBehaviour {
+
+	List<Transform> spawns = new List<Transform>();
+	public GameObject[] tetrominoArray = new GameObject[7];
+
+	private int lastSpawn = 0;
+	
+	void Start () {
+		foreach (Transform child in transform) {
+			spawns.Add(child);
+		}
+		InvokeRepeating ("spawnRandomTetromino", 0.0f, 1.0f);
+	}
+	
+	void spawnRandomTetromino(){
+		int spawnIndex = Random.Range (0, spawns.Count - 1);
+		while (spawnIndex == lastSpawn) {
+			spawnIndex = Random.Range (0, spawns.Count - 1);
+		}
+		int shapeIndex = Random.Range (0, tetrominoArray.Length - 1);
+		Instantiate(tetrominoArray[shapeIndex], spawns[spawnIndex].position, spawns[spawnIndex].rotation);
+		lastSpawn = spawnIndex;
+	}
+}
