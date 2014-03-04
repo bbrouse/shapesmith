67b57c59e2eddaa2eac7fa676de8969ca950d6e5
Ben Brouse <benjaminbrouse@gmail.com>
2014-03-04 06:21:39 -0500

    Added guide arrow along with 3D GUI prefab.

---
+++ shapesmith/Assets/Scripts/GuidePlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuidePlayer : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject reference;
+	public GameObject playerCamera;
+		
+	void Update () {
+		reference.transform.LookAt (target.transform.position);
+		//transform.rotation = Quaternion.FromToRotation(playerCamera.transform.forward, reference.transform.forward);
+		transform.rotation = Quaternion.Inverse (playerCamera.transform.rotation) * reference.transform.rotation;
+	}
+}
